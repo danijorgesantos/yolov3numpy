e296220766305ea0507cfc98c1cda3bea1771edc
Daniel Santos <daniel.santos@rupealgroup.onmicrosoft.com>
2020-07-22 01:27:19 +0100

    asd

--- test3.py
+++ test3.py
@@ -10,34 +10,35 @@ b2 = 1
 beta2 = 1
 beta = 1
 
-fcweighs1 = np.random.rand(input.shape[1],16)
+fcweighs1 = np.random.rand(input.shape[1],2)
 
 # --------------------------------------------------
 
-print('--------------------')
-print('wights',fcweighs1)
+# print('--------------------')
+# print('wights',fcweighs1)
 
 # feed foward layer 1 with activation function swish
 x = np.dot(input, fcweighs1)+b1
 layer1Result = (x * (1/(1 + np.exp(beta * -x))))
 
-print('----------------------------')
-print('layer 1 result --> ', layer1Result)
+# print('----------------------------')
+# print('layer 1 result --> ', layer1Result)
 
-fcweighs2 = np.random.rand(layer1Result.shape[1],16) 
+fcweighs2 = np.random.rand(layer1Result.shape[1],4) 
 
 # feed foward layer 2 with activation function swish
 x = np.dot(layer1Result, fcweighs2)+b1
 layer2Result = (x * (1/(1 + np.exp(beta * -x))))
 
+# print('----------------------------')
+# print('layer 2 result --> ', layer2Result)
+
 # feed foward layer 2 with activation function softmax for final layer
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0) 
 
+final = softmax(layer2Result[0])
 
-
-
-
-
-print('----------------------------')
-print('layer 2 result --> ', layer2Result)
-
+print(final)
--- xtrainnew.py
+++ xtrainnew.py
@@ -98,53 +98,6 @@ t = tqdm(imgArray, leave=True)
 #---------------------------------------------------------------------------------------------------------------------------------------
 
 
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-
-        self.weights1   = np.random.rand(self.input.shape[1],4) 
-        self.weights2   = np.random.rand(4,1)                 
-        self.y          = y
-        self.output     = np.zeros(self.y.shape)
-
-    def feedforward(self):
-
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1)+b1)
-        
-        self.output = sigmoid(np.dot(self.layer1, self.weights2)+b2)
-
-
-
-        # swish
-
-        # (x * (1/(1 + np.exp(beta * -x)))) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #---------------------------------------------------------------------------------------------------------------------------------------
 
 for image in t:
